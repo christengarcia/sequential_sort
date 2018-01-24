30d84b6f693cc672e76d1d2f9e3bd98dea1f3bf3
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-24 21:12:20 +1100

    Initial commit

---
+++ sequential_sort/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * C# Program to Implement Sequential Sort
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace sequential_sort
+{
+    public static void BubbleSort<T>(this List<T> list) where T : IComparable
+    {
+        bool changes;
+        int count = list.Count;
+        do
+        {
+            changes = false;
+            count--;
+            for (int i = 0; i < count; i++)
+            {
+                if (list[i].CompareTo(list[i + 1]) > 0)
+                {
+                    T temp = list[i + 1];
+                    list[i + 1] = list[i];
+                    list[i] = temp;
+                    changes = true;
+                }
+            }
+        } while (changes);
+    }
+}
+
+
+
